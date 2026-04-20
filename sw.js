2fcfd5e986062eaa282a6575ca680d3dee72cd78
Estrella00 <sunnyforest00@gmail.com>
2026-04-20 17:21:20 +0800

    Initial commit

---
+++ sw.js
@@ -0,0 +1,51 @@
+const CACHE = 'reading-log-v1';
+const SHELL = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './icons/apple-touch-icon.png'
+];
+
+// 安裝：快取 App Shell
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(SHELL))
+  );
+  self.skipWaiting();
+});
+
+// 啟用：刪除舊版快取
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// 攔截請求
+self.addEventListener('fetch', e => {
+  const url = new URL(e.request.url);
+
+  // Google Fonts：有網路就用新的，離線就用快取
+  if (url.hostname === 'fonts.googleapis.com' || url.hostname === 'fonts.gstatic.com') {
+    e.respondWith(
+      fetch(e.request)
+        .then(res => {
+          const clone = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, clone));
+          return res;
+        })
+        .catch(() => caches.match(e.request))
+    );
+    return;
+  }
+
+  // App Shell：優先用快取，沒有再去網路
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
